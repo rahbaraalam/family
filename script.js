1b3b7484ea5846962e182199b5cf24d5ef56984a
rahbaraalam <103810160+rahbaraalam@users.noreply.github.com>
2023-12-20 16:32:37 +0530

    family tree

---
+++ script.js
@@ -0,0 +1,50 @@
+function rahbar(){
+    document.getElementById("rahbar").innerText = "Sorry , No Data Found In This Profile.";
+}
+
+function mushaib(){
+    document.getElementById("mushaib").innerText = "Sorry , No Data Found In This Profile.";
+}
+
+function rifat(){
+    document.getElementById("rifat").innerText = "Sorry , No Data Found In This Profile.";
+}
+function rifa(){
+    document.getElementById("rifa").innerText = "Sorry , No Data Found In This Profile.";
+}
+function arsh(){
+    document.getElementById("arsh").innerText = "Sorry , No Data Found In This Profile.";
+}
+function kazim(){
+    document.getElementById("kazim").innerText = "Sorry , No Data Found In This Profile.";
+}
+function faiz(){
+    document.getElementById("faiz").innerText = "Sorry , No Data Found In This Profile.";
+}
+function adeeba(){
+    document.getElementById("adeeba").innerText = "Sorry , No Data Found In This Profile.";
+}
+function yasir(){
+    document.getElementById("yasir").innerText = "Sorry , No Data Found In This Profile.";
+}
+function liban(){
+    document.getElementById("liban").innerText = "Sorry , No Data Found In This Profile.";
+}
+function masheera(){
+    document.getElementById("masheera").innerText = "Sorry , No Data Found In This Profile.";
+}
+function aahil(){
+    document.getElementById("aahil").innerText = "Sorry , No Data Found In This Profile.";
+}
+function sakib(){
+    document.getElementById("sakib").innerText = "Sorry , No Data Found In This Profile.";
+}
+function ilma(){
+    document.getElementById("ilma").innerText = "Sorry , No Data Found In This Profile.";
+}
+function shawej(){
+    document.getElementById("shawej").innerText = "Sorry , No Data Found In This Profile.";
+}
+function mahira(){
+    document.getElementById("mahira").innerText = "Sorry , No Data Found In This Profile."
+}
